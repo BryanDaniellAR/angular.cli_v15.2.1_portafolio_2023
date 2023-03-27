902092b515043152e94db9ab7f16dcdc2180f1e7
Bryan Daniell <bryanbdar14@gmail.com>
2023-03-27 01:07:41 -0500

    version_1

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: false,
+    APP_URL:'https://bryanarrivasplata.com/website_apirest/api/'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    APP_URL:'https://bryanarrivasplata.com/website_apirest/api/'
+};
